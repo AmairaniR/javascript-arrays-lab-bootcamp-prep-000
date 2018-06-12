fdde9f2c43ea497c481ee4ce69affeace91cdba7
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-12 00:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Garfield", "Otis"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+
+  return kittens
+}
